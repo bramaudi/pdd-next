2e1203986bc5f5b7712033b0753b7837c4005c06
Brama Udi <bramaudi.al@gmail.com>
2020-09-11 15:03:12 +0700

    tambah package spatie

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Spatie Service Provider
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
