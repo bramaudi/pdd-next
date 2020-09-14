93af97c4a2115769760a7a8eacc2cfcf2e7c0d47
Brama Udi <bramaudi.al@gmail.com>
2020-09-14 14:27:00 +0700

    minor update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,17 @@
 <aside class="sidebar">
     @foreach($links as $link)
         @if(is_array($link['url']))
+            
             @if(isset($link['permission']))
-            @can($link['permission'])
-                @include('includes/sidebar-accordion', ['link' => $link])
-            @endcan
+                @can($link['permission'])
+                    @include('includes/sidebar-accordion', ['link' => $link])
+                @endcan
             @else
                 @include('includes/sidebar-accordion', ['link' => $link])
             @endif
+
         @else
+
             @if(isset($link['permission']))
                 @can($link['permission'])
                     @include('includes/sidebar-link', ['link' => $link])
@@ -16,6 +19,7 @@
             @else
                 @include('includes/sidebar-link', ['link' => $link])
             @endif
+
         @endif
     @endforeach
 </aside>
