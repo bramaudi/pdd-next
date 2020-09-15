2a89f290a32c356dc349683fe88e44ae09fbd6e4
Brama Udi <bramaudi.al@gmail.com>
2020-09-15 09:35:08 +0700

    tambah app icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@if(View::hasSection('title')) @yield('title') @else PDD @endif</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+    <link rel="manifest" href="/icons/site.webmanifest">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/icons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="msapplication-config" content="/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <livewire:styles />
+
+    <!-- Spectre -->
     <link rel="stylesheet" href="{{ asset('assets/css/spectre.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/icons.min.css') }}">
-    <!-- FontAwesome all -->
-    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/spectre-exp.min.css') }}">
+    
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
+    
     <link rel="stylesheet" href="{{ asset('assets/css/pdd.css') }}">
     <script src="{{ asset('assets/js/alpine.min.js') }}" defer></script>
 </head>
