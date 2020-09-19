dffcb1825a747f06f3bcef10eb746fa200e3fb12
Brama Udi <bramaudi.al@gmail.com>
2020-09-19 08:03:38 +0700

    fix bug

--- resources/views/includes/form-group-horizontal.blade.php
+++ resources/views/includes/form-group-horizontal.blade.php
@@ -1,9 +1,9 @@
 <div class="form-group @error($name) has-error @enderror">
     <div class="col-3 col-sm-12">
-        <label class="form-label" for="inp-{{ time() }}-{{ $name }}">{{ $label }}</label>
+        <label class="form-label" for="inp-{{ md5($name) }}-{{ $name }}">{{ $label }}</label>
     </div>
     <div class="col-9 col-sm-12">
-        <input class="form-input" type="{{ $type }}" id="inp-{{ time() }}-{{ $name }}" wire:model="{{ $name }}" placeholder="{{ $label }}">
+        <input class="form-input" type="{{ $type }}" id="inp-{{ md5($name) }}-{{ $name }}" wire:model="{{ $name }}" placeholder="{{ $label }}">
         @error($name) <div class="form-input-hint">{{ $message }}</div> @enderror
     </div>
 </div>
--- resources/views/includes/form-group.blade.php
+++ resources/views/includes/form-group.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group @error($name) has-error @enderror">
-    <label class="form-label" for="inp-{{ time() }}-{{ $name }}">{{ $label }}</label>
-    <input class="form-input" type="{{ $type }}" id="inp-{{ time() }}-{{ $name }}" wire:model="{{ $name }}" placeholder="{{ $label }}">
+    <label class="form-label" for="inp-{{ md5($name) }}-{{ $name }}">{{ $label }}</label>
+    <input class="form-input" type="{{ $type }}" id="inp-{{ md5($name) }}-{{ $name }}" wire:model="{{ $name }}" placeholder="{{ $label }}">
     @error($name) <div class="form-input-hint">{{ $message }}</div> @enderror
 </div>
