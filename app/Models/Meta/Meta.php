c3be8a40880efffd2e48d712cb68c7dda1b0131a
Wahyudi <cambagulung@gmail.com>
2020-09-20 16:43:45 +0800

    model Meta: menambahkan method decode

--- app/Models/Meta/Meta.php
+++ app/Models/Meta/Meta.php
@@ -3,8 +3,14 @@
 namespace App\Models\Meta;
 
 use Illuminate\Database\Eloquent\Model;
+use stdClass;
 
 class Meta extends Model
 {
     protected $fillable = ['key', 'description', 'value'];
+
+    public function decode(): stdClass
+    {
+        return json_decode($this->value);
+    }
 }
