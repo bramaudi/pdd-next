4012df29a158137fb151605f2cc0db202507452c
Brama Udi <bramaudi.al@gmail.com>
2020-09-21 06:31:02 +0700

    fixed, relasi model keluarga hasMany penduduk

--- app/Models/Penduduk/Keluarga.php
+++ app/Models/Penduduk/Keluarga.php
@@ -2,12 +2,19 @@
 
 namespace App\Models\Penduduk;
 
+use App\Models\Penduduk\Penduduk;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Keluarga extends Model
 {
     use HasFactory;
 
     protected $table = 'keluarga';
+
+    public function anggota(): HasMany
+    {
+        return $this->hasMany(Penduduk::class);
+    }
 }
