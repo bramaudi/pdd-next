a7ccff36064cb87088cab40d86e46bd780610e4b
Brama Udi <bramaudi.al@gmail.com>
2020-09-22 01:56:16 +0700

    tambah jumlah dummy KK

--- database/seeders/Kependudukan/KeluargaSeeder.php
+++ database/seeders/Kependudukan/KeluargaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders\Kependudukan;
 
 use App\Models\Kependudukan\Keluarga;
 use Illuminate\Database\Seeder;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class KeluargaSeeder extends Seeder
 {
@@ -14,10 +16,21 @@ class KeluargaSeeder extends Seeder
      */
     public function run()
     {
-        Keluarga::factory()
-            ->times(10)
-            ->hasAnggota(5)
-            ->create();
+        $times = 100;
+
+        $output = new ConsoleOutput();
+        $progressBar = new ProgressBar($output, $times);
+
+        for ($i = 0; $i < $times; $i++) {
+
+            Keluarga::factory()->hasAnggota(random_int(3, 10))->create();
+
+            $progressBar->advance();
+
+        }
+
+        $progressBar->finish();
+        $output->write("\n");
 
         $this->call(KepalaKeluargaSeeder::class);
     }
