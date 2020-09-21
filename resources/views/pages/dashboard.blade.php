5302682dcd9282acb94bcb91873e4ff710b30468
Brama Udi <bramaudi.al@gmail.com>
2020-09-22 01:59:56 +0700

    fungsional statistik

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,28 +5,28 @@
             <div class="stats" style="background: slateblue;">
                 <div class="title">Operator</div>
                 <i class="fas fa-cog"></i>
-                5
+                {{ $operator }}
             </div>
         </div>
         <div class="column col-3 col-sm-12">
             <div class="stats" style="background: teal;">
                 <div class="title">Penduduk</div>
                 <i class="fas fa-users"></i>
-                2056
+                {{ $penduduk }}
             </div>
         </div>
         <div class="column col-3 col-sm-12">
             <div class="stats" style="background: tomato;">
                 <div class="title">Keluarga</div>
                 <i class="fas fa-user-friends"></i>
-                1500
+                {{ $keluarga }}
             </div>
         </div>
         <div class="column col-3 col-sm-12">
             <div class="stats">
                 <div class="title">Wilayah Dusun</div>
                 <i class="fas fa-search-location"></i>
-                8
+                {{ $lingkungan }}
             </div>
         </div>
     </div>
