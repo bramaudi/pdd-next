88d12c7b99958639a7d3d2a562cdbfc8fa3e3a8f
Brama Udi <bramaudi.al@gmail.com>
2020-09-26 22:42:57 +0700

    fix minor

--- resources/views/livewire/role/delete.blade.php
+++ resources/views/livewire/role/delete.blade.php
@@ -2,8 +2,8 @@
     <div wire:loading.class.remove="d-none" class="loading loading-lg d-center d-none"></div>
     <div wire:loading.remove>
         Anda akan menghapus jabatan "<strong>{{ $name }}</strong>" ?
-        <br>
-        <br>
+    </div>
+    <div class="pt-2 mt-2">
         <button class="btn btn-error" wire:click="submit">Hapus</button>
     </div>
 </div>
