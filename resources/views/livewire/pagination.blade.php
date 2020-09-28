be8213763ddd4de71282a7d2a70fd15f21e12ead
Brama Udi <bramaudi.al@gmail.com>
2020-09-29 00:14:50 +0700

    optimize pagination

--- resources/views/livewire/pagination.blade.php
+++ resources/views/livewire/pagination.blade.php
@@ -1,63 +1,65 @@
-<ul class="pagination">
-@if ($paginator->hasPages())
-    
-    {{-- First Page --}}
-    @if ($paginator->onFirstPage())
-    <li class="page-item disabled">
-        <span>
-            {!! __('pagination.first') !!}
-        </span>
-    </li>
-    @else   
-    <li class="page-item">
-        <a class="c-hand" href="#first" wire:key="first" wire:click="gotoPage(1)" rel="first">
-            {!! __('pagination.first') !!}
-        </a>
-    </li>
-    @endif
-
-    @foreach ($elements as $element)
-        @if (is_array($element))
-            @foreach ($element as $page => $url)
-            @if ($paginator->currentPage() > 3 && $page === 2)
-                <li class="page-item">
-                    <span>...</span>
-                </li>
-                @endif
-
-                @if ($page == $paginator->currentPage())
-                <li class="page-item active" wire:key="{{ $page }}">
-                    <a>{{ $page }}</a>
-                </li>
-                @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2)
-                <li class="page-item c-hand" wire:key="{{ $page }}">
-                    <a href="#{{ $page }}" wire:click="gotoPage({{$page}})">{{ $page }}</a>
-                </li>
-                @endif
-
-                @if ($paginator->currentPage() < $paginator->lastPage() - 2  && $page === $paginator->lastPage() - 1)
-                <li class="page-item">
-                    <span>...</span>
-                </li>
-                @endif
-            @endforeach
+<nav>
+    <ul class="pagination">
+    @if ($paginator->hasPages())
+        
+        {{-- First Page --}}
+        @if ($paginator->onFirstPage())
+        <li wire:key="pagination-first" class="page-item disabled">
+            <span>
+                {!! __('pagination.first') !!}
+            </span>
+        </li>
+        @else   
+        <li wire:key="pagination-first" class="page-item">
+            <a class="c-hand" href="#first" wire:click="gotoPage(1)" rel="first">
+                {!! __('pagination.first') !!}
+            </a>
+        </li>
         @endif
-    @endforeach
     
-    {{-- Last Page --}}
-    @if ($paginator->hasMorePages())
-    <li class="page-item">
-        <a class="c-hand" href="#last" wire:key="last" wire:click="gotoPage({{ $paginator->lastPage() }})" rel="last">
-            {!! __('pagination.last') !!}
-        </a>
-    </li>
-    @else
-    <li class="page-item disabled">
-        <span>
-            {!! __('pagination.last') !!}
-        </span>
-    </li>
+        @foreach ($elements as $element)
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                @if ($paginator->currentPage() > 3 && $page === 2)
+                    <li wire:key="pagination-separate1" class="page-item">
+                        <span>...</span>
+                    </li>
+                    @endif
+    
+                    @if ($page == $paginator->currentPage())
+                    <li wire:key="pagination-page{{ $page }}" class="page-item active">
+                        <a href="#">{{ $page }}</a>
+                    </li>
+                    @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2)
+                    <li wire:key="pagination-page{{ $page }}" class="page-item c-hand">
+                        <a href="#{{ $page }}" wire:click="gotoPage({{$page}})">{{ $page }}</a>
+                    </li>
+                    @endif
+    
+                    @if ($paginator->currentPage() < $paginator->lastPage() - 2  && $page === $paginator->lastPage() - 1)
+                    <li wire:key="pagination-separate2" class="page-item">
+                        <span>...</span>
+                    </li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+        
+        {{-- Last Page --}}
+        @if ($paginator->hasMorePages())
+        <li wire:key="pagination-last" class="page-item">
+            <a class="c-hand" href="#last" wire:click="gotoPage({{ $paginator->lastPage() }})" rel="last">
+                {!! __('pagination.last') !!}
+            </a>
+        </li>
+        @else
+        <li wire:key="pagination-last" class="page-item disabled">
+            <span>
+                {!! __('pagination.last') !!}
+            </span>
+        </li>
+        @endif
+    
     @endif
-
-@endif
-</ul>
+    </ul>
+</nav>
