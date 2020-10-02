946e481547cfd6c130dc20f1a632d9be676b485b
Brama Udi <bramaudi.al@gmail.com>
2020-10-02 09:27:57 +0700

    rename

--- database/migrations/2020_09_24_221831_create_pengurus_table.php
+++ database/migrations/2020_09_24_221831_create_pengurus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePengurusesTable extends Migration
+class CreatePengurusTable extends Migration
 {
     /**
      * Run the migrations.
