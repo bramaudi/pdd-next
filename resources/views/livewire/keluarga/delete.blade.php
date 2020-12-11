17694ab0e61025452a9a3b629b57ea1f800c4833
Brama Udi <bramaudi.al@gmail.com>
2020-12-11 21:51:07 +0700

    done: menutup kekurangan

--- resources/views/livewire/keluarga/delete.blade.php
+++ resources/views/livewire/keluarga/delete.blade.php
@@ -3,9 +3,9 @@
     <div wire:loading.remove>
         Anda akan menghapus data keluarga: <br>
         No KK: <strong>{{ $no_kk }}</strong> <br>
-        Kepala Keluarga: {{ $nama_kepala }} ?
+        Kepala Keluarga: <strong>{{ $nama_kepala }}</strong> ?
     </div>
     <div class="pt-2 mt-2">
-        <button class="btn btn-error" wire:click="submit">Hapus</button>
+        <button class="btn bg-red-500" wire:click="submit">Hapus</button>
     </div>
 </div>
