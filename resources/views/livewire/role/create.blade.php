fc30c6624c66b8798ec211aab4a7e79cfa5a786f
Brama Udi <bramaudi.al@gmail.com>
2020-12-13 20:14:31 +0700

    finish: styling bagian terakhir ke tailwindcss

--- resources/views/livewire/role/create.blade.php
+++ resources/views/livewire/role/create.blade.php
@@ -1,13 +1,8 @@
 <form wire:submit.prevent="submit">
 
-    {{-- Nama Jabatan --}}
-    <div class="form-group @error('name') has-error @enderror">
-        <label class="form-label" for="inp-config-name">
-            Nama Jabatan:
-        </label>
-        <input wire:model="name" type="text" id="inp-config-password" class="form-input">
-        @error('name') <div class="form-input-hint">{{ $message }}</div> @enderror
-    </div>
+    <x-form-group model="name">
+        Nama Jabatan:
+    </x-form-group>
 
     <x-submit text="Buat" />
 
--- resources/views/livewire/user/permission.blade.php
+++ resources/views/livewire/user/permission.blade.php
@@ -1,59 +1,41 @@
-<div>
-    <div class="columns">
-        <div class="column col-6 col-sm-12">
-            <div class="card">
-                <div class="card-header"><strong>Desa</strong></div>
-                <div class="card-body">
-                    <label class="form-checkbox">
-                    <input wire:model="permissions.desa_identitas" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Identitas Desa
-                    </label>
-                    <label class="form-checkbox">
-                    <input wire:model="permissions.desa_wilayah" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Wilayah Administratif
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="column col-6 col-sm-12">
-            <div class="card">
-                <div class="card-header"><strong>Kependudukan</strong></div>
-                <div class="card-body">
-                    <label class="form-checkbox">
-                    <input wire:model="permissions.kependudukan_penduduk" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Penduduk
-                    </label>
-                    <label class="form-checkbox">
-                    <input wire:model="permissions.kependudukan_keluarga" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Keluarga
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="column col-12 mt-2">
-            <div class="card">
-                <div class="card-header"><strong>Sistem</strong></div>
-                <div class="card-body">
-                    <label class="form-checkbox">
-                    <input wire:model="permissions.sistem_pengguna" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Pengguna
-                    </label>
-                    <label class="form-checkbox">
-                        <input wire:model="permissions.sistem_jabatan" wire:click="changed" type="checkbox">
-                        <i class="form-icon"></i> Jabatan
-                    </label>
-                </div>
-            </div>
-        </div>
+<div class="card form">
+    <div class="section">Desa</div>
+    <div class="mb-3">
+        <label class="form-checkbox">
+            <input wire:model="permissions.desa_identitas" wire:click="changed" type="checkbox">
+            Identitas Desa
+        </label>
+        <label class="form-checkbox">
+            <input wire:model="permissions.desa_wilayah" wire:click="changed" type="checkbox">
+            Wilayah Administratif
+        </label>
+    </div>
+
+    <div class="section">Kependudukan</div>
+    <div class="mb-3">
+        <label class="form-checkbox">
+            <input wire:model="permissions.kependudukan_penduduk" wire:click="changed" type="checkbox">
+            Penduduk
+        </label>
+        <label class="form-checkbox">
+            <input wire:model="permissions.kependudukan_keluarga" wire:click="changed" type="checkbox">
+            Keluarga
+        </label>
     </div>
     
-    <div class="divider"></div>
+    <div class="section">Sistem</div>
+    <div class="mb-3">
+        <label class="form-checkbox">
+            <input wire:model="permissions.sistem_pengguna" wire:click="changed" type="checkbox">
+            Pengguna
+        </label>
+        <label class="form-checkbox">
+            <input wire:model="permissions.sistem_jabatan" wire:click="changed" type="checkbox">
+            Jabatan
+        </label>
+    </div>
+    
+    <x-submit text="Simpan" :disabled="!$changed" />
+    <button wire:click="resetChanges" class="btn">Reset</button>
 
-    @if(session()->has('success'))
-        <div class="toast toast-success">{{ session('success') }}</div>
-        <div class="divider"></div>
-    @endif
-
-    <button wire:click="submit" class="btn btn-lg btn-primary" @if(!$changed) disabled @endif>Simpan</button>
-    <button wire:click="resetChanges" class="btn btn-lg btn-link">Reset</button>
 </div>
