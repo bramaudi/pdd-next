467d2e25ec9a67b71644b40b04751e57ba8afd02
Brama Udi <bramaudi.al@gmail.com>
2020-12-13 19:39:42 +0700

    change: float header, fix modal

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -28,7 +28,7 @@
 
 <body x-data="{ sidebar: false }">
 
-    <header class="p-3 pr-1 flex items-center select-none bg-white shadow-md">
+    <header class="header">
         <button @click="sidebar = !sidebar" class="w-8 h-8 mr-3 border-2 border-transparent focus:outline-none">
             <svg x-show="sidebar" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
             <svg x-show="!sidebar" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
@@ -44,7 +44,7 @@
         <livewire:sidebar />
     </aside>
 
-    <main>
+    <main class="main">
         @yield('content')
     </main>
 
